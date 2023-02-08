d7612bb4a76fa766b1a9e733438ce411b993c3a1
Kritika Goyal <kritika_goyal@epam.com>
2023-02-08 17:41:36 +0530

    Implemented Both The ways

--- shufflearray/Arraytostring.js
+++
@@ -1,5 +0,0 @@
-function ArrayTostring(array) {
-    return array.toString();
-}
-const inputarray = ["h", "o", "l", "a"];
-console.log(`"${ArrayTostring(inputarray)}"`);
--- shufflearray/opositeNumber.js
+++
@@ -1,8 +0,0 @@
-function OppositeNumber(inputnumber){
-    inputnumber=inputnumber*(-1);
-    console.log((number));
-}
-    let input1 =5;
-    let input2=-34.14;
-    OppositeNumber(input1);
-    OppositeNumber(input2);
