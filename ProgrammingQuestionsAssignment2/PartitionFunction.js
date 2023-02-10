6e6b5d50109964814f2ca95b48310b939dd09eaf
Kritika Goyal <kritika_goyal@epam.com>
2023-02-10 17:05:12 +0530

    New assignment-2

---
+++ ProgrammingQuestionsAssignment2/PartitionFunction.js
@@ -0,0 +1,17 @@
+let itemsarray = [1, 2, 3, 4, 5, 6];
+function isEven(input) { return input % 2 == 0 }
+let newindex = partitionOn(isEven, itemsarray);
+console.log(newindex);
+function partitionOn(operation, items) {
+    let arrTrue = [];
+    let arrFalse = [];
+    for (let index = 0; index < itemsarray.length; index++) {
+        if (isEven(itemsarray[index])) arrTrue.push(itemsarray[index]);
+        else arrFalse.push(itemsarray[index]);
+    }
+    console.log(arrTrue);
+    console.log(arrFalse);
+    itemsarray = [...arrFalse, ...arrTrue];
+    console.log(itemsarray);
+    return arrFalse.length;
+}
