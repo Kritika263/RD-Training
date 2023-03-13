8e8a48ff655d050836c364b3cc8024b0066bd361
Kritika Goyal <kritika_goyal@epam.com>
2023-03-14 00:01:33 +0530

    NodeJs 1 & 2 Tasks

---
+++ NodeJsTask2/app.js
@@ -0,0 +1,59 @@
+console.log(process.argv);
+const operation = process.argv[3];
+console.log(operation);
+const inputArray = process.argv.slice(4).map(Number);
+let ans;
+switch (operation) {
+    case "addition":
+        if (inputArray.length >= 2) {
+            ans = inputArray.reduce((curr, prev) => {
+                curr += prev;
+                return curr;
+            }, 0);
+            console.log(ans);
+        }
+        else {
+            console.error("Invalid number of values");
+        }
+        break;
+    case "multiplication":
+        if (inputArray.length >= 2) {
+            let ans = inputArray.reduce((curr, prev) => {
+                curr *= prev;
+                return curr;
+            }, 1);
+            console.log(ans);
+        }
+        else {
+            console.error("Invalid number of values");
+        }
+        break;
+    case "subtraction":
+        if (inputArray.length == 2) {
+            let ans = inputArray[0] - inputArray[1];
+            console.log(ans);
+        }
+        else {
+            console.error("There must be 2 input values")
+        }
+        break;
+    case "division":
+        if (inputArray.length == 2 && inputArray[1] != 0) {
+            let ans = inputArray[0] / inputArray[1];
+            console.log(ans);
+        }
+        else {
+            if (inputArray.length != 2)
+                console.error("There must be 2 input values")
+            else if (inputArray[1] == 0)
+                console.error("Provide valid values");
+        }
+        break;
+    default:
+        console.log("Invalid operation");
+}
+
+
+
+
+
